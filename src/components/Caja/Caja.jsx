b382d409ec40d5388ef088d3f58a86040ca22bbc
Fabian F <noobseca123@hotmail.com>
2024-11-13 11:23:57 -0500

    update

--- src/components/Caja/Caja.jsx
+++ src/components/Caja/Caja.jsx
@@ -17,18 +17,20 @@ function Caja() {
     const obtenerEstadoCaja = async () => {
       try {
         const response = await fetch(`${backend}api/cajas/ultima-caja`);
-        if (!response.ok) throw new Error('Error al obtener el estado de la caja');
+        if (!response.ok)
+          throw new Error("Error al obtener el estado de la caja");
 
         const data = await response.json();
         setUltimaCaja(data.ultimaCaja); // Guarda la última caja
-       
+
         // Determina el tipo de caja para la siguiente operación
         if (data) {
-          setTipoCaja(data.ultimaCaja.tipoCaja === 'cierre' ? 'Cerrar' : 'Abrir');
+          setTipoCaja(
+            data.ultimaCaja.tipoCaja === "cierre" ? "Abrir" : "Cerrar"
+          );
         }
       } catch (error) {
-        console.error('Error:', error);
-        
+        console.error("Error:", error);
       }
     };
 
@@ -45,22 +47,60 @@ function Caja() {
         <button onClick={toggleMenu}>
           <img src={CajaRegistradora} alt="" className="size-20" />
         </button>
-        <div className={`${isMenuVisible ? 'block' : 'hidden'} absolute bg-white translate-y-20 mt-36 md:mt-20 w-56 md:w-[550px]  xl:h-44 border-4 rounded-xl p-2 z-20`}>
+        <div
+          className={`${
+            isMenuVisible ? "block" : "hidden"
+          } absolute bg-white translate-y-20 mt-36 md:mt-20 w-56 md:w-[550px]  xl:h-44 border-4 rounded-xl p-2 z-20`}
+        >
           <div className="grid grid-cols-2 md:grid-cols-4 gap-4 font-bold text-xs xl:text-sm text-center">
-            <Link to="/AddClient" className="border-4 p-1 rounded-xl hover:scale-110 duration-200">
-              <img src={Client} alt="Agregar Cliente" className="size-10 xl:size-20 mx-auto" />
-              <span>Agregar Cliente</span>
-            </Link>
-            <Link to="/ContadorBilletes" className={`${tipoCaja === 'Abrir' ? 'bg-blue-300' : 'bg-red-300'} border-4 p-1 rounded-xl hover:scale-110 duration-200`}>
-              <img src={Money} alt="Caja" className="size-10 xl:size-20 mx-auto" />
+            {tipoCaja === "Cerrar" ? (
+              <Link
+                to="/AddClient"
+                className="border-4 p-1 rounded-xl hover:scale-110 duration-200"
+              >
+                <img
+                  src={Client}
+                  alt="Agregar Cliente"
+                  className="size-10 xl:size-20 mx-auto"
+                />
+                <span>Agregar Cliente</span>
+              </Link>
+            ) : (
+              <div className="border-4 p-1 m-auto"><span>Abre la caja Para agregar Clientes</span></div> 
+            )}
+            <Link
+              to="/ContadorBilletes"
+              className={`${
+                tipoCaja === "Cerrar" ? "bg-red-300" : "bg-blue-300"
+              } border-4 p-1 rounded-xl hover:scale-110 duration-200`}
+            >
+              <img
+                src={Money}
+                alt="Caja"
+                className="size-10 xl:size-20 mx-auto"
+              />
               <span>{tipoCaja} Caja</span>
             </Link>
-            <Link to="/Ventas" className="border-4 p-1 rounded-xl hover:scale-110 duration-200">
-              <img src={Ventas} alt="Ventas" className="size-10 xl:size-20 mx-auto" />
+            <Link
+              to="/Ventas"
+              className="border-4 p-1 rounded-xl hover:scale-110 duration-200"
+            >
+              <img
+                src={Ventas}
+                alt="Ventas"
+                className="size-10 xl:size-20 mx-auto"
+              />
               <span>Ventas</span>
             </Link>
-            <Link to="/Inventories" className="border-4 p-1 rounded-xl hover:scale-110 duration-200">
-              <img src={Inventory} alt="Agregar Inventario" className="size-10 xl:size-20 mx-auto" />
+            <Link
+              to="/Inventories"
+              className="border-4 p-1 rounded-xl hover:scale-110 duration-200"
+            >
+              <img
+                src={Inventory}
+                alt="Agregar Inventario"
+                className="size-10 xl:size-20 mx-auto"
+              />
               <span>Agregar Inventario</span>
             </Link>
           </div>
@@ -71,7 +111,6 @@ function Caja() {
         <div className="col-span-2 border-4 bg-slate-300"></div>
         <div className="col-span-1 border-4 bg-slate-300"></div>
       </div>
-     
     </div>
   );
 }
